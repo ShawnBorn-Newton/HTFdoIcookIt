afce7cfa52fb5820f3f526fa3df4b532274ce894
ShawnBorn-Newton <shawn.newton3@gmail.com>
2017-08-21 15:07:47 -0400

    first commit

---
+++ HTFdoIcookIt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HTFdoIcookIt.Startup))]
+namespace HTFdoIcookIt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
